9c243c54919660d9e83b73330df2307a74480c9d
Darwin  R. Oliva <droliva@uth.hn>
2025-09-30 18:47:36 -0600

    Creando Index.html
    Base de Tarea

---
+++ src/main/java/com/proyectnumeros/www/app_numerosd/HelloServlet.java
@@ -0,0 +1,29 @@
+package com.proyectnumeros.www.app_numerosd;
+
+import java.io.*;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("Llamada del servel");
+        out.println("</body> </html>");
+    }
+
+    public void destroy() {
+    }
+}
